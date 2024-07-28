e1bb9ff4f2bdbd63a260fe16f1bca5023b96406e
t1mofey4 <timofeyvalerievich@gmail.com>
2024-07-29 00:54:59 +0300

    code refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -8,54 +8,20 @@ import (
 	"net"
 	"time"
 
-	desc "github.com/Timofey335/auth/pkg/authservice_v1"
 	"github.com/brianvoe/gofakeit"
 	"github.com/fatih/color"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
+
+	desc "github.com/Timofey335/auth/pkg/authservice_v1"
 )
 
 const grpcPort = 50051
 
 type server struct {
 	desc.UnimplementedAuthserviceV1Server
-}
-
-func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	log.Println(color.BlueString("Create user: %v, with ctx: %v", req, ctx))
-	return &desc.CreateResponse{
-		Id: gofakeit.Int64(),
-	}, nil
-}
-
-func getUserRole() desc.Role {
-	roles := []desc.Role{desc.Role_UNKNOW, desc.Role_USER, desc.Role_ADMIN}
-	rand.NewSource(time.Now().UnixNano())
-	randomRole := rand.Intn(len(roles))
-	return roles[randomRole]
-}
-
-func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
-	log.Println(color.BlueString("Note id: %d", req.GetId()))
-	return &desc.GetResponse{
-		Id:        gofakeit.Int64(),
-		Name:      gofakeit.Name(),
-		Email:     gofakeit.Email(),
-		Role:      getUserRole(),
-		CreatedAt: timestamppb.New(gofakeit.Date()),
-		UpdatedAt: timestamppb.New(gofakeit.Date()),
-	}, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	log.Println(color.HiMagentaString("Delete user: id %d, with ctx: %v", req.Id, ctx))
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
-	return &emptypb.Empty{}, nil
 }
 
 func main() {
@@ -73,4 +39,47 @@ func main() {
 	if err := s.Serve(lis); err == nil {
 		log.Fatalf(color.RedString("failed to serve: %v", err))
 	}
+}
+
+// CreateUser - create a new user
+func (s *server) CreateUser(ctx context.Context, req *desc.CreateUserRequest) (*desc.CreateUserResponse, error) {
+	log.Println(color.BlueString("Create user: %v, with ctx: %v", req, ctx))
+
+	return &desc.CreateUserResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+func getUserRole() desc.Role {
+	roles := []desc.Role{desc.Role_UNKNOWN, desc.Role_USER, desc.Role_ADMIN}
+	rand.NewSource(time.Now().UnixNano())
+	randomRole := rand.Intn(len(roles))
+
+	return roles[randomRole]
+}
+
+// GetUser - get information of the user by id
+func (s *server) GetUser(ctx context.Context, req *desc.GetUserRequest) (*desc.GetUserResponse, error) {
+	log.Println(color.BlueString("Note id: %d", req.GetId()))
+
+	return &desc.GetUserResponse{
+		Id:        gofakeit.Int64(),
+		Name:      gofakeit.Name(),
+		Email:     gofakeit.Email(),
+		Role:      getUserRole(),
+		CreatedAt: timestamppb.New(gofakeit.Date()),
+		UpdatedAt: timestamppb.New(gofakeit.Date()),
+	}, nil
+}
+
+// DeleteUser - delete a user by id
+func (s *server) DeleteUser(ctx context.Context, req *desc.DeleteUserRequest) (*emptypb.Empty, error) {
+	log.Println(color.HiMagentaString("Delete user: id %d, with ctx: %v", req.Id, ctx))
+
+	return &emptypb.Empty{}, nil
+}
+
+// UpdateUser - update information of the user by id
+func (s *server) UpdateUser(ctx context.Context, req *desc.UpdateUserRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
 }
