41535e2258fe5c40bd6ef89aca0589a971eea6bc
Timofey335 <timofeyvalerievich+git@gmail.com>
2024-07-31 20:17:46 +0300

    added docker-compose and  migrations

---
+++ migrations/20240731_create_users_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+create table users (
+    id serial primary key,
+    name text not null UNIQUE,
+    email text not null UNIQUE,
+    password text not null,
+    role text not null,
+    created_at timestamp not null default now(),
+    updated_at timestamp
+);
+
+-- +goose Down
+drop table users;
