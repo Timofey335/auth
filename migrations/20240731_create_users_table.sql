0479b7a779f6266729f8956c1551653f6381c73a
Timofey335 <timofeyvalerievich+git@gmail.com>
2024-08-03 23:44:23 +0300

    Changed type of the field "role" from int to text

--- migrations/20240731_create_users_table.sql
+++ migrations/20240731_create_users_table.sql
@@ -4,7 +4,7 @@ create table users (
     name text not null UNIQUE,
     email text not null UNIQUE,
     password text not null,
-    role text not null,
+    role int not null,
     created_at timestamp not null default now(),
     updated_at timestamp
 );
