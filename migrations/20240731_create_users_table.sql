7c840727068eccce28e432e1022b839b7a9a8703
Timofey335 <timofeyvalerievich+git@gmail.com>
2024-08-18 15:38:18 +0300

    corrected the migration file

--- migrations/20240731_create_users_table.sql
+++ migrations/20240731_create_users_table.sql
@@ -1,13 +1,13 @@
 -- +goose Up
-create table users (
-    id serial primary key,
-    name text not null UNIQUE,
-    email text not null UNIQUE,
-    password text not null,
-    role int not null,
-    created_at timestamp not null default now(),
-    updated_at timestamp
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    role INT NOT NULL,
+    created_at TIMESTAMP NOT NULL default now(),
+    updated_at TIMESTAMP
 );
 
 -- +goose Down
-drop table users;
+DROP TABLE users;
